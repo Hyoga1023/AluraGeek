2615533191dbe73c09b7eb1b8a4f9bb5c126761b
Cesar Martinez <cesarmartinez99@gmail.com>
2024-11-14 22:13:37 -0500

    actualiza data contextos

--- js/Interactividad.js
+++ js/Interactividad.js
@@ -2,17 +2,14 @@ const productos = document.getElementById('productos');
 const formulario = document.getElementById('formulario');
 const body = document.body;
 
-// Función para cambiar el contexto
 function cambiarContexto(contexto) {
   body.setAttribute('data-contexto', contexto);
 }
 
-// Evento para la sección de productos
 productos.addEventListener('click', () => {
   cambiarContexto('pagina_principal');
 });
 
-// Evento para la sección de formulario
 formulario.addEventListener('click', () => {
   cambiarContexto('Estado de compras');
 });
