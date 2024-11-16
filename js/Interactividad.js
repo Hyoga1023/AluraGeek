b9015e4fab9113f20ef0017ece60bff85bdb52b9
Cesar Martinez <cesarmartinez99@gmail.com>
2024-11-15 20:55:04 -0500

    crea volver arriba y cambio data contexto principal

--- js/Interactividad.js
+++ js/Interactividad.js
@@ -4,27 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const trashIcons = document.querySelectorAll('.trashIcon');
   const botonVolverArriba = document.getElementById('volver-arriba');
 
-  // Función para cambiar el contexto
   function cambiarContexto(contexto) {
     body.setAttribute('data-contexto', contexto);
   }
 
-  // Evento para el enlace de funcionalidad
   funcionalidadEnlace.addEventListener('click', () => {
     cambiarContexto('Estado de compras');
     document.getElementById('texto').scrollIntoView({ behavior: 'smooth' });
   });
 
-  // Eventos para iconos de eliminación
   trashIcons.forEach(icono => {
     icono.addEventListener('click', () => {
       cambiarContexto('pagina_principal');
     });
   });
 
-  // Manejo del botón de volver arriba
   function manejarBotonVolverArriba() {
-    // Mostrar u ocultar el botón basado en la posición del scroll
+
     if (window.pageYOffset > 200) {
       botonVolverArriba.classList.add('mostrar');
     } else {
@@ -32,14 +28,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  // Agregar evento de scroll
   window.addEventListener('scroll', manejarBotonVolverArriba);
 
-  // Evento de clic para volver arriba
   botonVolverArriba.addEventListener('click', () => {
     window.scrollTo({ 
       top: 0, 
       behavior: 'smooth' 
     });
+
+    cambiarContexto('pagina_principal');
   });
 });
